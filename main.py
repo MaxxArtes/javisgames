41314e14209648e22a354243e68ca72fd0aa943d
maxuel henrique paula campos <124100812+MaxxArtes@users.noreply.github.com>
2026-01-10 15:36:08 -0300

    Update main.py

--- main.py
+++ main.py
@@ -39,8 +39,8 @@ app.add_middleware(
         "https://www.javisgameacademy.com.br",
         "http://localhost:3000",
         "http://localhost:5173",
-        "http://localhost:8080"
-        "http://localhost:5500"
+        "http://localhost:8080",
+        "http://localhost:5500",
         "http://127.0.0.1:5500"
     ], 
     allow_credentials=True,
